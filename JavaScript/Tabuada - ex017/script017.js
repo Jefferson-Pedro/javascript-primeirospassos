b2c9d2801e162bb3642fadae5380f856e33d0336
Jefferson Pedro <jeffersonpedro05@gmail.com>
2021-03-19 13:01:26 -0300

    Projeto-aula

--- JavaScript/Tabuada - ex017/script017.js
+++ JavaScript/Tabuada - ex017/script017.js
@@ -1,10 +1,17 @@
 function calc(){
-    let numero = document.getElementsById('n1')
-    let select = document.getElementById('seltab')
-    let res = document.getElementById('op')
+    let numero = document.getElementById('n1')
+    let tab = document.getElementById('seltab')
+   
 
-    let num = Number(numero.value)
-    for(let c = 1; c <= 10; c++){
-        res.innerHTML = `${num} x ${c} = ${num*c}`
-    }
+    if(numero.value.length == 0){
+        window.alert('[ERRO] Insira um numero para começarmos!')
+    }else{
+        let num = Number(numero.value)
+        tab.innerHTML = ''
+        for(let c = 0; c <= 10; c++ ){
+            let item = document.createElement('option')
+            item.text = `${num} x ${c} = ${c*num}` 
+            tab.appendChild(item)
+        }
+    } 
 }
